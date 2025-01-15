dc74ca39d437070c0fecef33b19398545597cc4f
LavdrimIslamiTU <tuo97292@temple.edu>
2025-01-15 13:55:49 -0500

    Finished random.c file

--- print_random.c
+++ print_random.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include "random.c"
 
 int main()
 {
 	int a;
 	/* seed the randomizer */
 	srand( (unsigned)time(NULL) );
-
 	printf("Today's random word: ");
 	for(a=0;a<7;a++)
 		putchar( randchar() );
 	putchar('\n');
-	printf("hello");
+	
 
 	return(0);
 }
--- random.c
+++ random.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 
 int randchar(){
-    int val = rand();
+    int lower = 65;
+    int upper = 90;
+    int value = rand() % (upper - lower + 1) + lower;
+    //65 - 90 is ascii for uppercase alphabet
+
+    return value;
+
 
 }
